6b9eaeeff1baba32908ecdf76493559b09a9f4fa
Gunaseelan A <gunasiet@gmail.com>
2015-10-12 21:46:25 +0530

    Initial commit

---
+++ app/src/main/java/com/guna/colorpickerexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.guna.colorpickerexample;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.guna.libcolorpicker.ColorPickerOval;
+import com.guna.libcolorpicker.ColorPickerRect;
+
+public class MainActivity extends AppCompatActivity implements ColorPickerOval.OnColorChangedListener, ColorPickerRect.OnColorChangedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void openPicker1(View view) {
+        new ColorPickerOval(this, this, Color.WHITE).show();
+    }
+
+    public void openPicker2(View view) {
+        new ColorPickerRect(this, this, "", Color.BLACK, Color.WHITE).show();
+    }
+
+    @Override
+    public void colorChanged(int color) {
+        this.findViewById(android.R.id.content)
+                .setBackgroundColor(color);
+    }
+
+    @Override
+    public void colorChanged(String key, int color) {
+        this.findViewById(android.R.id.content)
+                .setBackgroundColor(color);
+    }
+}
